9f1304d1b80a76813af08318627f602878f377bf
Jhonattan Willian Correia Silva <jhonattanwcs@hotmail.com>
2024-11-27 10:12:39 -0300

    correção da api

--- src/servicos/api.js
+++ src/servicos/api.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api', // Base URL
+  baseURL: process.env.REACT_APP_API_BASE_URL, // Base URL configurada em .env
   timeout: 20000, // 20 segundos
 });
+
+// Exibe a baseURL configurada no console para depuração
+console.log('Base URL da API:', process.env.REACT_APP_API_BASE_URL);
 
 export default api;
